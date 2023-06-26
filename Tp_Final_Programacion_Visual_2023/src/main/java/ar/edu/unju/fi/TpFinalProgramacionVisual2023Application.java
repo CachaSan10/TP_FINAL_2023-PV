dfdffef21f2edd322976c88ed140e4d617c7e04e
Nahuel Alberto Cachambi <cachasan.10@gmail.com>
2023-06-26 20:55:52 -0300

    Responsable: Nahuel Alberto Cachambi user: CachaSan10 Descripción: Preparacion del proyecto spring.

---
+++ Tp_Final_Programacion_Visual_2023/src/main/java/ar/edu/unju/fi/TpFinalProgramacionVisual2023Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalProgramacionVisual2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalProgramacionVisual2023Application.class, args);
+	}
+
+}
