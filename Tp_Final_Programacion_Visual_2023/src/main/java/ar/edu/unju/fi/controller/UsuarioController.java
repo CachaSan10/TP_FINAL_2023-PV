b90af9d65815ada45fd853363e13ab221ea4f88d
VillenaFranco <franco.fv322@gmail.com>
2023-06-27 21:29:36 -0300

    user: VillenaFranco Description: Se agrego controller para gestionar paginas de Usuario. Close #9

---
+++ Tp_Final_Programacion_Visual_2023/src/main/java/ar/edu/unju/fi/controller/UsuarioController.java
@@ -0,0 +1,71 @@
+package ar.edu.unju.fi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.unju.fi.entity.Usuario;
+import ar.edu.unju.fi.service.IUsuarioService;
+
+@Controller
+@RequestMapping("/registrarme")
+public class UsuarioController {
+	@Autowired
+	private IUsuarioService usuarioService;
+	
+	@Autowired
+	private Usuario usuario;
+	
+	@GetMapping("/nuevo")
+	public String getNuevoUsuarioPage(Model model) {
+		boolean edicion=false;
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("edicion", edicion);
+		return "nuevo_usuario";
+	}
+	
+	@PostMapping("/guardar")
+	public ModelAndView getGuardarUsuarioPage(@ModelAttribute("sucursal") Usuario usuario) {
+		ModelAndView mav = new ModelAndView("usuarios");
+		
+		usuarioService.guardarUsuario(usuario);
+		return mav;
+	}
+
+	@GetMapping("/modificar/{id}")
+	public String getModificarUsuarioPage(Model model, @PathVariable(value="id")Long id) {
+		boolean edicion=true;
+		Usuario usuarioEncontrado = usuarioService.buscarUsuario(id);
+		
+		model.addAttribute("usuario", usuarioEncontrado);
+		model.addAttribute("edicion", edicion);
+		return "nuevo_usuario";
+	}
+	
+	@PostMapping("/modificar/{id}")
+	public String modificarUsuario(@ModelAttribute("usuario") Usuario usuarioModificado) {
+		usuarioService.modificarUsuario(usuarioModificado);
+		return "redirect:/inicio";
+	}
+	
+	public String eliminarUsuario(@PathVariable(value="id")Long id) {
+		Usuario usuarioEncontrado = usuarioService.buscarUsuario(id);
+		usuarioService.eliminarUsuario(usuarioEncontrado);
+		
+		return "";
+	}
+
+	@GetMapping("/listado")
+	public String getUsuariosPage(Model model) {
+		model.addAttribute("usuarios", usuarioService.obtenerLista());
+		return "usuarios";
+	}
+
+
+}
