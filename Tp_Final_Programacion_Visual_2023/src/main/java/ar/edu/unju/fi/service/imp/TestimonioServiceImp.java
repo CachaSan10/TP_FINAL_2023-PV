1a2dfff90beea37dfbd03c24a51bcd354a79400a
Lautaro Nahuel <lautaromoyata.lm@gmail.com>
2023-06-29 05:32:19 -0300

    Responsable: Moyata, Lautaro Nahuel
    User: Nahuel277
    Description: modificacion de las capas repository, service y serviceImp
    para testimonio.
    Close #34

--- Tp_Final_Programacion_Visual_2023/src/main/java/ar/edu/unju/fi/service/imp/TestimonioServiceImp.java
+++ Tp_Final_Programacion_Visual_2023/src/main/java/ar/edu/unju/fi/service/imp/TestimonioServiceImp.java
@@ -1,47 +1,97 @@
 package ar.edu.unju.fi.service.imp;
 
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import ar.edu.unju.fi.entity.Testimonio;
 import ar.edu.unju.fi.repository.ITestimonioRepository;
 import ar.edu.unju.fi.service.ITestimonioService;
+import ar.edu.unju.fi.util.UploadFile;
 
+
+@Service("testimonioServiceListaImp")
 public class TestimonioServiceImp implements ITestimonioService{
-	
+    
 	@Autowired
 	private ITestimonioRepository testimonioRepository;
 	
+	@Autowired
+	private UploadFile uploadFile;
+	
+	@Autowired
+	private Testimonio testimonio;
 	
 	@Override
-	public void agregarTestimonio(Testimonio testimonio) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void guardarTestimonio(Testimonio testimonio) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void modificarTestimonio(Testimonio testimoino) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void eliminarTestimonio(int id) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public List<Testimonio> obtenerTodosLosTestimonios() {
-		// TODO Auto-generated method stub
-		return null;
+		return testimonioRepository.findByEstado(true);
 	}
 
+	@Override
+	public Testimonio obtenerTestimonio() {
+		return testimonio;
+	}
+
+	@Override
+	public void agregarTestimonio(Testimonio testimonio, MultipartFile imagen) throws IOException {
+		testimonio.setFecha(LocalDate.now());
+		String uniqueFileName = uploadFile.copy(imagen);
+		testimonio.setEstado(true);
+		System.out.println(testimonio.toString());
+		testimonioRepository.save(testimonio);
+	}
+
+	@Override
+	public Testimonio obtenerTestimonioEncontrado(Long id) {
+		Testimonio testimonioEncontrado = new Testimonio();
+		testimonioEncontrado.setId(id);
+		testimonioEncontrado.setFecha(testimonioRepository.findById(id).get().getFecha());
+		testimonioEncontrado.setEstado(true);
+		testimonioEncontrado.setImagen(testimonioRepository.findById(id).get().getImagen());
+		testimonioEncontrado.setUsuario(testimonioRepository.findById(id).get().getUsuario());
+		testimonioEncontrado.setComentario(testimonioRepository.findById(id).get().getComentario());
+		
+		System.out.println("testimonio encontrado:" + testimonioRepository.findById(id).get().toString());
+		return testimonioEncontrado;
+	}
+
+	@Override
+	public void actualizarTestimonio(Testimonio testimonioModificado, MultipartFile imagen) throws IOException {
+		Testimonio unTestimonio = new Testimonio();
+		System.out.println("testimonio encontrado: " + testimonioModificado.toString());
+		System.out.println("testimonio encontrado2: " + testimonioRepository.findById(testimonioModificado.getId()).get().toString());
+		unTestimonio.setId(testimonioModificado.getId());
+		unTestimonio.setEstado(true);
+		unTestimonio.setFecha(LocalDate.now());
+		unTestimonio.setUsuario(testimonioModificado.getUsuario());
+		unTestimonio.setComentario(testimonioModificado.getComentario());
+		
+		if (!imagen.isEmpty()) {
+			String imagenString = imagen.getOriginalFilename();
+
+			if (imagenString.compareTo(obtenerTestimonioEncontrado(testimonioModificado.getId()).getImagen()) != 0) {
+				uploadFile.delete(obtenerTestimonioEncontrado(testimonioModificado.getId()).getImagen());
+				imagenString = uploadFile.copy(imagen);
+				unTestimonio.setImagen(imagenString);
+			}
+		}else {
+			unTestimonio.setImagen(obtenerTestimonioEncontrado(testimonioModificado.getId()).getImagen());
+		}
+		
+		testimonioRepository.save(unTestimonio);
+	}
+
+	@Override
+	public void eliminarTestimonio(Long id) {
+		Testimonio otroTestimonio = new Testimonio();
+		otroTestimonio = testimonioRepository.findById(id).get();
+		uploadFile.delete(otroTestimonio.getImagen());
+		otroTestimonio.setEstado(false);
+		testimonioRepository.save(otroTestimonio);
+	}
+	
 }
