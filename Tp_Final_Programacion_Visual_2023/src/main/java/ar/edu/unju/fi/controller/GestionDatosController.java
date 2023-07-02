8a511223a4d4b01df2622e528e8c0584582f5c0b
Nahuel Alberto Cachambi <cachasan.10@gmail.com>
2023-07-02 15:45:59 -0300

    Responsable: Nahuel Alberto Cachambi user: CachaSan10 Descripción: Se cambio de metodo post a get  en la interfaz de autenticar administrador, buscar_peso_ideal, calculadora_imc, registros imc.

--- Tp_Final_Programacion_Visual_2023/src/main/java/ar/edu/unju/fi/controller/GestionDatosController.java
+++ Tp_Final_Programacion_Visual_2023/src/main/java/ar/edu/unju/fi/controller/GestionDatosController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ar.edu.unju.fi.entity.Usuario;
@@ -24,7 +23,7 @@ public class GestionDatosController {
         return "autenticacion";
     }
     
-    @PostMapping("/autenticar")
+    @GetMapping("/autenticar")
     public String postAutenticacion(Long id,Model model) {
         if (usuarioService.existeUsuario(id) && usuarioService.buscarUsuario(id).isAdministrador()==true) {
         	
