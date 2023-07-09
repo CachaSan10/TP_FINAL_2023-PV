bc3e47e75b67ddb355799ed5d75c7a47e66da0c3
VillenaFranco <franco.fv322@gmail.com>
2023-07-09 14:14:55 -0300

    Responsable: Villena Franco user: VillenaFranco Description: Se Agrego documentacion a UsuarioController ademas se modifico vista en formulario gestion de testimonio y nuevo testimonio. Close #121

--- Tp_Final_Programacion_Visual_2023/src/main/java/ar/edu/unju/fi/controller/UsuarioController.java
+++ Tp_Final_Programacion_Visual_2023/src/main/java/ar/edu/unju/fi/controller/UsuarioController.java
@@ -24,6 +24,11 @@ public class UsuarioController {
 	private IUsuarioService usuarioService;
 	
 	
+	/**
+	 * Metodo que retorna  formulario para registrar usuarios
+	 * @param model representa la clase model para enviar las varibles edicion y usuario
+	 * @return la pagina nuevo_usuario.html
+	 */
 	@GetMapping("/nuevo")
 	public String getNuevoUsuarioPage(Model model) {
 		boolean edicion=false;
@@ -32,6 +37,13 @@ public class UsuarioController {
 		return "nuevo_usuario";
 	}
 	
+	/**
+	 * Metodo que guarda al usuario en la base de datos
+	 * @param representa al usuario que se envio para guardar los datos
+	 * @param bindingResult representa la clase que recibe los errores del formulario
+	 * @return la pagina agradecimiento en caso de no tener errores al cargar el formulario,
+	 * si captura errores mostrara nuevamente el formulario de registro
+	 */
 	@PostMapping("/guardar")
 	public ModelAndView postGuardarUsuarioPage(@Valid @ModelAttribute("usuario") Usuario usuario, BindingResult bindingResult) {
 
@@ -53,6 +65,12 @@ public class UsuarioController {
 	}
 
 
+	/**
+	 * Metodo que retorna el formulario de usuarios para modificar los datos
+	 * @param model representa la clase model para enviar usuario, edicion
+	 * @param id representa el id de usuario que se va a modificar
+	 * @return el formulario para modificar nuevo_usuario.html
+	 */
 	@GetMapping("/modificar/{id}")
 	public String getModificarUsuarioPage(Model model, @PathVariable(value="id")Long id) {
 		boolean edicion=true;
@@ -63,6 +81,13 @@ public class UsuarioController {
 		return "nuevo_usuario";
 	}
 	
+	/**
+	 * Metodo que modifica los datos del usuario
+	 * @param usuarioModificado representa el usuario modificado
+	 * @param bindingResult representa la clase que recibe los errores del formulario
+	 * @return la pagina con todos los usuarios registrados, si encuentra algun error
+	 *  vuelve a retornar el formulario para modificar
+	 */
 	@PostMapping("/modificar/{id}")
 	public String modificarUsuario(@Valid @ModelAttribute("usuario") Usuario usuarioModificado, BindingResult bindingResult) {
 		if ( bindingResult.hasErrors() ) {
@@ -73,6 +98,11 @@ public class UsuarioController {
 		return "redirect:/usuario/listado";
 	}
 	
+	/**
+	 * Metodo que elimina el usuario de la base de datos
+	 * @param id representa el id del usuario a eliminar
+	 * @return la pagina en donde se encuentra el listado de usuarios
+	 */
 	@GetMapping("/eliminar/{id}")
 	public String eliminarUsuario(@PathVariable(value="id")Long id) {
 		Usuario usuarioEncontrado = usuarioService.buscarUsuario(id);
@@ -81,6 +111,11 @@ public class UsuarioController {
 		return "redirect:/usuario/listado";
 	}
 
+	/**
+	 * Metodo que devuelve el listado de usuarios para gestionar sus datos(modificar/eliminar)
+	 * @param model representa la clase que envia los usuarios
+	 * @return la pagina usuarios.html
+	 */
 	@GetMapping("/listado")
 	public String getUsuariosPage(Model model) {
 		model.addAttribute("usuarios", usuarioService.obtenerLista());
